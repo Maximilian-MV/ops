a11f613c70b67d750623527ee49673bb0d20b17e
Maximilian-MV <88027447+Maximilian-MV@users.noreply.github.com>
2021-08-03 11:36:18 +0300

    Changes

---
+++ src/pages/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Input, Button, Section } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { RawHtml } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Input
+			disabled-text-transform="uppercase"
+			color="#5749f7"
+			padding="16px 30px 6px 16px"
+			position="relative"
+			top="px"
+			bottom="-1.5px"
+			text-align="left"
+			left="5px"
+			type="file"
+		/>
+		<Button position="relative" left="20px">
+			Чтение Файла
+		</Button>
+		<Section position="relative" top="px" left="300px" bottom="600px" />
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+		<RawHtml>
+			<style place={"endOfHead"} rawKey={"6107a25baa9cc700185a4cd2"}>
+				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
+			</style>
+		</RawHtml>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,31 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6107a25baa9cc700185a4cd7",
+				"6107a25baa9cc700185a4cd9"
+			]
+		},
+		"6107a25baa9cc700185a4cd7": {
+			"id": "6107a25baa9cc700185a4cd7",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6107a25baa9cc700185a4cd9": {
+			"id": "6107a25baa9cc700185a4cd9",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {
+			"background": "--axyenna"
+		},
+		"seo": {}
+	}
+}
